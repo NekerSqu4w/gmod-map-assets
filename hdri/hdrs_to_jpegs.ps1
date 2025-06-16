0cf0d75f43b6433343642acc225657135e98f6aa
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2025-06-16 03:36:41 +0200

    ew

--- hdri/hdrs_to_jpegs.ps1
+++ hdri/hdrs_to_jpegs.ps1
@@ -1,20 +1,40 @@
 $inputDir = "./hdr"
 $outputDir = "./jpg"
 
-# delete old file because ffmpeg need confirmation when rewriting a file
-# there is probably a way to remove confirmation with ffmpeg idk
-Get-ChildItem -Path $outputDir -Filter "*.jpg" | ForEach-Object {
-    $_.Delete()
+# make the folder if outputDir doesn't exist
+if (-not (Test-Path $outputDir)) {
+    New-Item -ItemType Directory -Path $outputDir | Out-Null
 }
 
-
-# convert all file
+# verify for each files if already converted or not
+# if not, convert it
 Get-ChildItem -Path $inputDir -Filter "*.hdr" | ForEach-Object {
-    $inputPath = $_.DirectoryName
-    $inputFile = $inputPath + "\" + $_.Name
-
-    # can be probably optimised
-    $outputFile = $inputPath.Substring(0, $inputPath.LastIndexOf('\')) + $outputDir + "\" + $_.Name.Substring(0, $_.Name.LastIndexOf('.')) + ".jpg"
-     
-    ffmpeg -i $inputFile -vf "eq=gamma=1.9:contrast=1.1,format=rgb24" -q:v 2 $outputFile
+    $inputFile = $_.FullName
+    $outputFileName = [System.IO.Path]::ChangeExtension($_.Name, ".jpg")
+    $outputFile = Join-Path $outputDir $outputFileName
+	
+    if (Test-Path $outputFile) {
+        Write-Host "skip conversion for $outputFileName" -ForegroundColor Cyan
+    }
+    else {
+        Write-Host "converting file $outputFileName" -ForegroundColor Yellow
+        
+		$startTime = Get-Date
+        & ffmpeg -loglevel error -i $inputFile -vf "format=rgb24" -q:v 2 $outputFile
+        $endTime = Get-Date
+        $duration = $endTime - $startTime
+		
+        if ($LASTEXITCODE -eq 0) {
+			$timeTaken = [math]::Round($duration.TotalMilliseconds)
+            Write-Host "conversion done for $outputFileName in $timeTaken ms" -ForegroundColor Green
+        }
+        else {
+            Write-Host "conversion failed for $outputFileName" -ForegroundColor Red
+        }
+    }
+	
+    Start-Sleep -Milliseconds 20 # just in case we have alot of files
 }
+
+Write-Host "all conversions done." -ForegroundColor Magenta
+pause
